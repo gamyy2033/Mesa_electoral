8ea18d42e32bfd41dd3b63e52ebd95b353ece8f9
gamyy2033 <adrianconrado2003@gmail.com>
2025-02-10 23:07:41 -0500

    Update settings.py

--- Mesa/settings.py
+++ Mesa/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d-m-l4tj+)!z)0#eysno$ck^-%bl7%yld$hwgn^49rh!u+n(!9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['Mesa.onrender.com']
 
